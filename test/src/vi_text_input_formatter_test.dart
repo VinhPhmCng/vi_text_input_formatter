7c63791a6314a5e52e793776e1bba72c97ef11ed
VPC <111203113+VinhPhmCng@users.noreply.github.com>
2026-01-16 12:52:31 +0700

    Add simple tests

--- test/src/vi_text_input_formatter_test.dart
+++ test/src/vi_text_input_formatter_test.dart
@@ -1,13 +1,49 @@
 // Not required for test files
-// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/semantics.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:vi_text_input_formatter/vi_text_input_formatter.dart';
 
 void main() {
   group('ViTextInputFormatter', () {
-    test('can be instantiated', () {
-      expect(ViTextInputFormatter(), isNotNull);
+    late ViTextInputFormatter formatter;
+
+    setUp(() {
+      formatter = ViTextInputFormatter();
+    });
+
+    test('correctly formats insertion', () {
+      const oldValue = TextEditingValue(
+        text: 'khóa',
+        selection: TextSelection.collapsed(offset: 4),
+      );
+      const newValue = TextEditingValue(
+        text: 'khóan',
+        selection: TextSelection.collapsed(offset: 5),
+      );
+      const expectedValue = TextEditingValue(
+        text: 'khoán',
+        selection: TextSelection.collapsed(offset: 5),
+      );
+      final result = formatter.formatEditUpdate(oldValue, newValue);
+      expect(result, equals(expectedValue));
+    });
+
+    test('correctly formats deletion', () {
+      const oldValue = TextEditingValue(
+        text: 'khoán',
+        selection: TextSelection.collapsed(offset: 5),
+      );
+      const newValue = TextEditingValue(
+        text: 'khoá',
+        selection: TextSelection.collapsed(offset: 4),
+      );
+      const expectedValue = TextEditingValue(
+        text: 'khóa',
+        selection: TextSelection.collapsed(offset: 4),
+      );
+      final result = formatter.formatEditUpdate(oldValue, newValue);
+      expect(result, equals(expectedValue));
     });
   });
 }
